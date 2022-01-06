c7b91e8519730607210f0027b58489c9e8745ea4
Vikas Jyani <vickbishnoi@gmail.com>
2022-01-07 05:02:16 +0530

    added: fetching random meme from a list of arrray

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Components/Header";
+import Meme from "./Components/Meme";
 
 function App() {
   return (
-    <p>
+    <div>
       <Header />
-    </p>
+      <Meme />
+    </div>
   );
 }
 
---
+++ src/Components/Meme.js
@@ -0,0 +1,28 @@
+import React from "react";
+import memesData from "../memesData";
+
+export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+    // could also be done this way
+    // const {url} = memesArray[randomNumber]
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        {/* <br></br> */}
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={memeImage} className="meme--image" />
+    </main>
+  );
+}
